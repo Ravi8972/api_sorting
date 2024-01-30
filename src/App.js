99eecbe89b349f37e38eebe4a545c6cd79b0007a
Raviranjan Mishra <ravimishra.rm9558@gmail.com>
2024-01-30 13:45:32 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React , {useState} from "react";
 
 function App() {
+
+  const APIURL ="https://jsonplaceholder.typicode.com/users";
+  const[users,setUsers] = useState([]);
+  const [sort , setSort] = useState(0);
+
+  const getDetails = () =>{
+    return fetch(APIURL)
+    .then((response)=>response.json())
+    .then((data)=>setUsers(data));
+    }
+    
+   const SortData = () =>{
+    console.log(users)
+    if(sort === 0 || sort === 2){
+      setUsers([...users].sort((a,b)=> a.name.length - b.name.length));
+      setSort(1);
+    }
+    else {
+      setUsers([...users].sort((a,b)=> b.name.length - a.name.length));
+      setSort(2);
+    }
+   }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>User Details</h1>
+      <div>
+        <button onClick={getDetails}> User Details </button>
+        <button onClick = {SortData} > Sort Data</button>
+      </div>
+      <ul>
+        {
+          users.map((user)=>
+          (<li key={user.Id}> Name : {user.name} , {user.name.length} </li>))
+        }
+        
+      </ul>
     </div>
   );
 }
 
 export default App;
+
+
+
